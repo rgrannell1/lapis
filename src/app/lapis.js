66e6b8c02f115afcd7f330bb0723969597268a73
Róisín Grannell <r.grannell2@gmail.com>
2020-07-11 20:10:43 +0100

    split files

--- src/app/lapis.js
+++ src/app/lapis.js
@@ -1,14 +1,13 @@
 
 const { codes } = require('../commons/constants')
 
-const errors = require('@rgrannell/errors')
 const simpleGit = require('simple-git')
 const path = require('path')
-const sqlite3 = require('sqlite3')
-const url = require('url')
 const tmp = require('tmp-promise')
 const fs = require('fs').promises
 const fse = require('fs-extra')
+
+const validateRepo = require('./validate-repo')
 
 const getRepoState = async () => {
   const status = await git.status()
@@ -16,56 +15,16 @@ const getRepoState = async () => {
   console.log(status.not_added)
 }
 
-const validateRepo = async args => {
-  let repo
-  const parsed = url.parse(args.repo)
-
-  // -- check the repo status
-  if (parsed.protocol === 'https') {
-    repo = {
-      protocol: 'https',
-      fpath: args.repo
-    }
-  } else if (parsed.protocol === null) {
-    let stat
-    try {
-      stat = await fs.lstat(args.repo)
-    } catch (err) {
-      throw errors.invalidRepoProtocol(`failed to stat repository "${args.repo}"`, codes.INVALID_REPO)
-    }
-
-    if (!stat.isDirectory()) {
-      throw errors.invalidRepoProtocol(`"${args.repo}" was not a directory`, codes.INVALID_REPO)
-    }
-
-    repo = {
-      protocol: 'file',
-      fpath: args.repo
-    }
-  } else {
-    throw errors.invalidRepoProtocol('repo must be a folder or https address', codes.INVALID_REPO)
-  }
-
-  if (repo.protocol === 'https') {
-    throw errors.invalidRepoProtocol('https protocol not yet supported for repos', codes.UNSUPPORTED)
-  }
-
-  const git = simpleGit(repo.fpath)
-
-  // -- check that the folder actually is a git repo.
-  await git.status()
-
-  return repo
+const runBenchmarks = fpath => {
+  
 }
 
-const lapis = async rawArgs => {
-  const args = lapis.preprocess(rawArgs)
-
+const createRepoClone = async args => {
   // -- copy .git to a temporary directory.
-  const repo = await validateRepo(args)
+  const repoStatus = await validateRepo(args)
   const { path: fpath, cleanup } = await tmp.dir()
 
-  const gitPath = path.join(args.repo, '.git')
+  const gitPath = path.join(repoStatus.fpath, '.git')
   const targetGitPath = path.join(fpath, '.git')
   
   await fs.mkdir(targetGitPath)
@@ -75,6 +34,15 @@ const lapis = async rawArgs => {
   const git = simpleGit(fpath)
   await git.reset('hard')
 
+  return fpath
+}
+
+const lapis = async rawArgs => {
+  const args = lapis.preprocess(rawArgs)
+
+  const fpath = await createRepoClone(args)
+
+  runBenchmarks(fpath)
 }
 
 lapis.preprocess = rawArgs => {
---
+++ src/app/validate-repo.js
@@ -0,0 +1,51 @@
+
+const { codes } = require('../commons/constants')
+
+const errors = require('@rgrannell/errors')
+const simpleGit = require('simple-git')
+const url = require('url')
+const fs = require('fs').promises
+
+const validateRepo = async args => {
+  let repo
+  const parsed = url.parse(args.repo)
+
+  // -- check the repo status
+  if (parsed.protocol === 'https') {
+    repo = {
+      protocol: 'https',
+      fpath: args.repo
+    }
+  } else if (parsed.protocol === null) {
+    let stat
+    try {
+      stat = await fs.lstat(args.repo)
+    } catch (err) {
+      throw errors.invalidRepoProtocol(`failed to stat repository "${args.repo}"`, codes.INVALID_REPO)
+    }
+
+    if (!stat.isDirectory()) {
+      throw errors.invalidRepoProtocol(`"${args.repo}" was not a directory`, codes.INVALID_REPO)
+    }
+
+    repo = {
+      protocol: 'file',
+      fpath: args.repo
+    }
+  } else {
+    throw errors.invalidRepoProtocol('repo must be a folder or https address', codes.INVALID_REPO)
+  }
+
+  if (repo.protocol === 'https') {
+    throw errors.invalidRepoProtocol('https protocol not yet supported for repos', codes.UNSUPPORTED)
+  }
+
+  const git = simpleGit(repo.fpath)
+
+  // -- check that the folder actually is a git repo.
+  await git.status()
+
+  return repo
+}
+
+module.exports = validateRepo
