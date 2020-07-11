90fdfe6084a704707f60d169f08fc523b26d6b67
Róisín Grannell <r.grannell2@gmail.com>
2020-07-11 23:26:45 +0100

    added comments

--- src/app/lapis.js
+++ src/app/lapis.js
@@ -91,7 +91,16 @@ const createRepoClone = async args => {
   return fpath
 }
 
+/**
+ * Run benchmark set and collect data.
+ * 
+ * @param {String} opts.name            the benchmark-set's name 
+ * @param {Function} opts.benchmark     the benchmark- 
+ * @param {String} opts.benchmarkName   the benchmark name 
+ */
 const runBenchmark = async ({ name, benchmarkName, benchmark }) => {
+
+
 
 }
 
